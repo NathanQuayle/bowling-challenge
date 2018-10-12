d719b5958fe4224d3ac04f3068eaca76218fe0aa
Nathan Quayle <nathan.quayle@gmail.com>
2018-10-12 21:08:47 +0100

    Bowling class works

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,21 +1,49 @@
 describe('Bowling', () => {
     beforeEach(() => {
-        this.bowling = new Bowling();
+        this.bowling = new Bowling;
     });
 
-    describe('initialize', () => {
-        it('should start with an empty scorecard', () => {
-            expect(this.bowling.frames.length).toEqual(10);
+    describe('.add', () => {
+        it('should add numbers in the first frame', () => {
+            bowling.add(1);
+            expect(this.bowling.frames[0][0]).toBe(1);
+        });
+
+        it('should call .nextBowl() if frame is < 10 and bowl < 1', () => {
+            spyOn(bowling, 'nextBowl');
+
+            bowling.add(1);
+
+            expect(bowling.nextBowl).toHaveBeenCalled();
+        });
+
+        it('should call .nextFrame() if frame is < 10 and bowl is 1', () => {
+            spyOn(bowling, 'nextFrame');
+
+            bowling.add(1);
+            bowling.add(1);
+
+            expect(bowling.nextFrame).toHaveBeenCalled();
         });
     });
 
-    describe('update', () => {
-        it('should update the correct .frames object', () => {
-            // returns a random integer from 1 to 10
-            let num = Math.floor(Math.random() * 10) + 1;
-            expect(this.bowling.frames[3][1]).toEqual(0);
-            this.bowling.update(3, 1, num);
-            expect(this.bowling.frames[3][1]).toEqual(num);
+    describe('.nextBowl', () => {
+        it('should increment the current bowl', () => {
+            bowling.nextBowl(1);
+            expect(this.bowling.currentBowl).toBe(1);
+        });
+    });
+
+    describe('.nextFrame', () => {  
+        it('should increment the current frame', () => {
+            bowling.nextFrame(1);
+            expect(this.bowling.currentFrame).toBe(1);
+        });
+
+        it('should reset currentBowl to 0', () => {
+            bowling.currentBowl = 1;
+            bowling.nextFrame()
+            expect(bowling.currentBowl).toBe(0);
         });
     });
 });
--- src/scripts/Bowling.js
+++ src/scripts/Bowling.js
@@ -1,5 +1,9 @@
 class Bowling {
     constructor() {
+        this.totalScore = 0;
+        this.currentFrame = 0;
+        this.currentBowl = 0;
+        this.liveGame = true;
         this.frames = [
             [0, 0],
             [0, 0],
@@ -10,11 +14,27 @@ class Bowling {
             [0, 0],
             [0, 0],
             [0, 0],
-            [0, 0]
+            [0, 0, 0]
         ];
     }
 
-    update(frame, roll, score) {
-        this.frames[frame][roll] = score; 
+    add(roll) {
+        this.frames[this.currentFrame][this.currentBowl] = roll;
+        if (this.currentFrame < 10 && this.currentBowl == 1) {
+            this.nextFrame();
+        }
+
+        if (this.currentFrame < 10 && this.currentBowl < 2) {
+            this.nextBowl();
+        }
+    }
+
+    nextBowl() {
+        this.currentBowl++;
+    }
+
+    nextFrame() {
+        this.currentFrame++;
+        this.currentBowl = 0;
     }
 }
